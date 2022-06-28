771872fd4e335ea1bad9bbca56729e2cbd892346
tejaroykorukonda <97439402+tejaroykorukonda@users.noreply.github.com>
2022-06-28 23:25:30 +0530

    def perfect_num(N):    sum = 0    for i in range(1,N):       if(N%i == 0):          sum = sum+i    return sum N = int(input()) if(N == perfect_num(N)):    print("True") else:    print("False")

--- Perfect_Number.py
+++ Perfect_Number.py
@@ -1,11 +1,11 @@
-def perfect_numbers(N): 
+def perfect_num(N):
    sum = 0
    for i in range(1,N): 
       if(N%i == 0):
          sum = sum+i 
    return sum
 N = int(input())
-if(N == perfect_numbers(N)): 
+if(N == perfect_num(N)): 
    print("True") 
 else: 
-   print("False") 
+   print("False")
