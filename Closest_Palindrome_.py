f285bdabb526aebe1aacbb1e43b472d8d1ca8013
tejaroykorukonda <97439402+tejaroykorukonda@users.noreply.github.com>
2022-06-28 23:47:40 +0530

    def palin(n):     m=n     s=0     while m!=0:         v = m%10         s = s*10+v         m = m//10     if n==s:         return n x = int(input()) for i in range(x-1,1,-1):     if palin(i):         a=i         break y = x+1 while y!=0:     if palin(y):         b=y         break     y+=1 if (x-a)<(b-x):     print(a) elif (x-a)==(b-x):     print(a,b) else:     print(b)

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,24 +1,24 @@
-def p(n):
+def palin(n):
     m=n
     s=0
     while m!=0:
-        v=m%10
-        s=s*10+v
-        m=m//10
+        v = m%10
+        s = s*10+v
+        m = m//10
     if n==s:
         return n
-x=int(input())
+x = int(input())
 for i in range(x-1,1,-1):
-    if p(i):
+    if palin(i):
         a=i
         break
-y=x+1
+y = x+1
 while y!=0:
-    if p(y):
+    if palin(y):
         b=y
         break
     y+=1
-if(x-a)<(b-x):
+if (x-a)<(b-x):
     print(a)
 elif (x-a)==(b-x):
     print(a,b)
