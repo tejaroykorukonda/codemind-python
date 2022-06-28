eb355f940b5cfaf21e60717e8b3b255a4cd57c6b
tejaroykorukonda <97439402+tejaroykorukonda@users.noreply.github.com>
2022-06-28 23:32:45 +0530

    x = (input()) s="" for i in x:     if i!='-' and i!='0':         s=i+s     elif i=='-':         print("-",end="") print(s)

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,8 +1,8 @@
-x=input()
-s=''
+x = (input())
+s=""
 for i in x:
-    if i!='-'and i!='0':
+    if i!='-' and i!='0':
         s=i+s
     elif i=='-':
-        print('-',end='')
+        print("-",end="")
 print(s)
